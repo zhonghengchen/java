be845b9245c98e3c4c8cc5bc100534d24dbde196
zhonghengchen <76793988+zhonghengchen@users.noreply.github.com>
2024-09-02 22:39:28 -0400

    Create guessnumber.java

---
+++ guessnumber.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		Random random = new Random();
+
+		int x = random.nextInt(100) + 1;
+
+		Scanner input = new Scanner(System.in);
+		int a;
+
+		do {
+			System.out.println("Guess number from 1 to 100: ");
+			a = input.nextInt();
+
+			if (a > x) {
+				System.out.println("Too High！");
+			}
+			if (a < x) {
+				System.out.println("Too Low！");
+			}
+
+		} while (a != x);
+
+		System.out.println("Correct！");
+
+	}
+
+}
