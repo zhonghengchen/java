745d8605ee713d0bff66e62295a6ef0dd56be940
zhonghengchen <76793988+zhonghengchen@users.noreply.github.com>
2024-09-03 22:50:24 -0400

    Update Guessnumber.java

--- Lab2/Guessnumber.java
+++ Lab2/Guessnumber.java
@@ -1,3 +1,7 @@
+/*
+Write a guessing game program. The program should pick a random integer from 1 to 100. Then it asks the user for a number and says too low, too high, or correct. The user should be able to keep entering numbers until he/she answers correctly.
+*/
+
 import java.util.Random;
 import java.util.Scanner;
 
