826beb446506bf26e9a0a74838e9f905b747b70e
zhonghengchen <76793988+zhonghengchen@users.noreply.github.com>
2024-09-03 22:51:00 -0400

    Update SumofDigits.java

--- Lab2/SumofDigits.java
+++ Lab2/SumofDigits.java
@@ -1,3 +1,6 @@
+/*
+Write a static method that has an integer as a parameter and returns the sum of the digits in that number. Then write code in a main method to test it.
+*/
 import java.util.Scanner;
 
 public class SumofDigits {
