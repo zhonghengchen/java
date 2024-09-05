215b085f84215e7e676045ddf9d45e81ce8d8458
zhonghengchen <76793988+zhonghengchen@users.noreply.github.com>
2024-09-05 10:21:24 +0800

    Create makeChocolate.java

---
+++ codingbat/makeChocolate.java
@@ -0,0 +1,12 @@
+/*
+We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+makeChocolate(4, 1, 9) → 4
+makeChocolate(4, 1, 10) → -1
+makeChocolate(4, 1, 7) → 2
+*/
+public int makeChocolate(int small, int big, int goal) {
+  if (goal > big * 5 + small) {return -1;}
+  if (goal % 5 > small )  {return -1;}
+  if (goal - big * 5 <0) {return goal%5;}
+  return goal - big *5;
+}
