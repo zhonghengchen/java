876c4d0205a9aa7a5fe002dda62fd68c34a0e345
zhonghengchen <76793988+zhonghengchen@users.noreply.github.com>
2024-09-05 10:42:35 +0800

    Update fizzString.java

--- codingbat/fizzString.java
+++ codingbat/fizzString.java
@@ -17,3 +17,14 @@ public String fizzString(String str) {
     }
     return result;
 }
+
+/*
+public String fizzString(String str) {
+  boolean f = str.startsWith("f");
+  boolean b = str.endsWith("b");
+  
+  if (f && b) return "FizzBuzz";
+  if (f) return "Fizz";
+  if (b) return "Buzz";
+  return str;
+*/
